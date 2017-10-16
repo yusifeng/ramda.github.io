39a3a6982893e95f6705e844421c5fbd66dd5583
王增迪 <sunnyadi@163.com>
2017-10-16 21:50:18 +0800

    change get_ramda_file.js

--- get_ramda_file.js
+++ get_ramda_file.js
@@ -3,9 +3,6 @@ var fs = require('fs')
 var path = require('path')
 
 module.exports = (child_path) => new Promise ((resolve, reject) => {
-  var ramda_module_path = require.resolve('ramda')
-
-  var ramda_folder = path.dirname(ramda_module_path)
   ramda_folder = path.resolve('../ramda')
 
   var file_path = path.join(ramda_folder, child_path)
